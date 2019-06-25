684b8c5726f2923ec24700394214a571af1efc40
Richard Gross <richard.gross@maibornwolff.de>
2019-06-25 23:08:46 +0200

    *** add coroutine test that does not work yet

--- src/test/kotlin/de/richargh/sandbox/kt/mvn/concurrency/CoroutineTest.kt
+++ src/test/kotlin/de/richargh/sandbox/kt/mvn/concurrency/CoroutineTest.kt
@@ -1,15 +1,17 @@
 package de.richargh.sandbox.kt.mvn.concurrency
 
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestReporter
+import org.junit.jupiter.api.fail
 
 class CoroutineTest {
 
     @Test
-    fun `test routines`(testReporter: TestReporter) = runBlocking {
+    fun `test routines`(testReporter: TestReporter) = runBlocking<Unit> {
         testReporter.publishEntry("Start")
 
         // Start a coroutine
@@ -20,5 +22,19 @@ class CoroutineTest {
 
         delay(2_000) // wait for 2 seconds
         testReporter.publishEntry("Stop")
+    }
+
+    @Test
+    fun `trying to cancel the job before fail is printed`(testReporter: TestReporter) = runBlocking<Unit> {
+        launch {
+            // arrange
+            val job = coroutineContext[Job]
+
+            // act
+            job?.cancel()
+
+            // assert
+            fail("this should not happen, because we did cancel the job")
+        }
     }
 }
